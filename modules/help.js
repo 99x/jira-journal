03e248c9803b16c6c1cd8bad5524b668f3ac987e
kosalanuwan <kosala.nuwan@gmail.com>
2017-04-20 21:37:24 +0530

    Include a temp logo for bot.

--- modules/help.js
+++ modules/help.js
@@ -8,12 +8,12 @@ module.exports = exports = [(session) => {
         .title('JIRA Journal Bot')
         .text('Your bullet journal - whatever you want to log.')
         .images([
-            builder.CardImage.create(session, 'http://docs.botframework.com/images/demo_bot_image.png')
+            builder.CardImage.create(session, 'https://bot-framework.azureedge.net/bot-icons-v1/jira-journal_1kD9TrAk04dW4K92uPBis7Bk2wP1rT3VI6NMBk95Lo7TC3BU.png')
         ]);
 
     const reply = new builder.Message(session)
         .attachmentLayout(builder.AttachmentLayout.list)
         .attachments([card]);
     session.send(reply);
-    session.endDialog('Hi... I\'m the JIRA Journal Bot for time reporting. Thanks for adding me. Say \'help\' to see what I can do');
+    session.endDialog('Hi... I\'m the JIRA Journal Bot for time reporting. Thanks for adding me. Say \'help\' to see what I can do!');
 }];
