8e238d41b802a5d82dede83964a29452819bc89e
kosalanuwan <kosala.nuwan@gmail.com>
2017-04-20 19:15:22 +0530

    Fixing string format issue.

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const bot = require('./modules/bot');
 const server = restify.createServer();
 server.post('/bot', bot.connector('*').listen());
 server.listen(process.env.PORT, () => {
-    console.log('${server.name} listening to ${server.url}');
+    console.log('%s listening to %s', server.name, server.url);
 });
