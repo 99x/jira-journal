461ee0042fd7b23b0d6d5d0021a9a92829b01cee
kosalanuwan <kosala.nuwan@gmail.com>
2017-04-23 00:45:26 +0530

    Fixing the terminating dialog issues. Using endConversation and replaceDialog to end and restart the waterfall process. Using magic string concatination. Rephrasing messages sent to the user. Refactor logic to improve readability.

--- modules/signin.js
+++ modules/signin.js
@@ -1,90 +1,102 @@
 'use strict';
 
 const builder = require('botbuilder');
+
 const timesheet = require('./timesheet');
 const sendmail = require('./sendmail');
 const secrets = require('./secrets');
 
+const emailValidator = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
 module.exports = exports = [(session) => {
 
-        builder.Prompts.text(session, 'What\'s your company email address?');
+    const name = session.message.user.name;
+    builder.Prompts.text(session, 'What\'s the company email, ${name}?');
 
-    }, (session, results, next) => {
+}, (session, results, next) => {
 
-        if (results.response && /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(results.response)) {
+    const email = session.privateConversationData.email = results.response;
 
-            session.dialogData.email = results.response;
-            session.dialogData.secretCode = secrets.whisper();
-
-            next();
-            return;
-        }
-
-        session.endDialog('That\'s not an email address!');
-
-    }, (session, results, next) => {
-
-        const email = session.dialogData.email;
-        const secretCode = session.dialogData.secretCode;
-
-        const draft = {
-            from: 'Bot <noreply@jirajournal.io>',
-            to: session.message.user.name + ' <' + email + '>',
-            subject: 'Your Secret Code ' + secretCode,
-            text: 'You just tryed to sign in with JIRA Journal. Here\'s your Secret Code: ' + secretCode,
-            html: 'You just tryed to sign in with JIRA Journal. Here\'s your <b>Secret Code: </b>' + secretCode
-        };
-
-        session.sendTyping();
-
-        sendmail.compose(draft)
-            .then(() => {
-                session.send('Ok. Dropped an email to %s.', results.response);
-                next();
-
-            }).catch((ex) => {
-                console.log(ex);
-                session.endDialog();
-            });
-
-    }, (session) => {
-
-        builder.Prompts.text(session, 'What\'s the *secret code* in it?');
-
-    }, (session, results, next) => {
-
-        if (results.response && results.response === session.dialogData.secretCode) {
-            next();
-            return;
-        }
-
-        session.endDialog('That\'s not what I sent you! :P');
-
-    }, (session, results, next) => {
-
-        session.sendTyping();
-
-        timesheet
-            .searchColleagues(results.response)
-            .then((colleagues) => {
-                console.log('Found %s', colleagues.length);
-
-                session.userData = colleagues[0];
-                session.userData.impersonated = true;
-                next();
-
-            }).catch((ex) => {
-                console.log(ex);
-                session.endDialog();
-            });
-    },
-
-    (session) => {
-
-        if (session.userData.impersonated) {
-            session.endDialog('(y)');
-            return;
-        }
-        session.endDialog('Oops! Couldn\'t impersonate');
+    if (!emailValidator.test(email)) {
+        session.send('${email}\'s not an email address! Let\'s start over.');
+        return session.replaceDialog('/signin');
     }
-];
+
+    next();
+
+}, (session) => {
+
+    const name = session.message.user.name;
+    const email = session.privateConversationData.email;
+    const secretCode = session.privateConversationData.secretCode = secrets.whisper();
+
+    const draft = {
+        from: 'Bot <noreply@jirajournal.io>',
+        to: name + ' <' + email + '>',
+        subject: 'Your Secret Code ' + secretCode,
+        text: 'You just tryed to sign in with JIRA Journal. Here\'s your Secret Code: ' + secretCode,
+        html: 'You just tryed to sign in with JIRA Journal. Here\'s your <b>Secret Code: </b>' + secretCode
+    };
+
+    console.log('Email template: %s', draft);
+
+    session.sendTyping();
+
+    sendmail.compose(draft)
+        .then(() => {
+
+            console.log('Dropped an email to %s', email);
+
+            session.send('Ok. I dropped you a mail to ${email}.');
+            next();
+
+        }).catch((ex) => {
+
+            console.log('Failed with error: %s', ex.message);
+
+            session.send('Oops! Something went wrong with the email. Shame on us (facepalm). Let\'s start over.');
+            session.replaceDialog('/signin');
+        });
+
+}, (session) => {
+
+    builder.Prompts.text(session, 'What\'s the *secret code* in it?');
+
+}, (session, results, next) => {
+
+    const secretCode = session.privateConversationData.secretCode;
+    const confirmCode = results.response;
+
+    if (confirmCode !== secretCode) {
+        session.send('That\'s not the Secret Code I sent you (wonder). Let\'s start over.');
+        return session.replaceDialog('/signin');
+    }
+
+    next();
+
+}, (session) => {
+
+    const email = session.privateConversationData.email;
+
+    session.sendTyping();
+
+    timesheet
+        .searchColleagues(email)
+        .then((colleagues) => {
+
+            console.log('Found %s', colleagues.length);
+
+            session.userData = colleagues[0];
+            session.userData.impersonated = true;
+
+            session.endConversation('(y)');
+
+        }).catch((ex) => {
+
+            console.log('Failed with error: %s', ex.message);
+
+            session.send('Oops! Something went wrong. Shame on us (facepalm).');
+            session.send('Sorry! Let\'s start over.');
+            session.replaceDialog('/signin');
+        });
+}];
