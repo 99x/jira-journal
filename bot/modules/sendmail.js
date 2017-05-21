257b87ffab9afe9b9b547fdf168b0a77cbb24047
kosalanuwan <kosala.nuwan@gmail.com>
2017-05-21 13:16:38 +0530

    add environment variables for snailmail.

--- bot/modules/sendmail.js
+++ bot/modules/sendmail.js
@@ -6,7 +6,7 @@ const options = {
     host: 'smtp.office365.com',
     post: 587,
     auth: {
-        user: process.env.SMTP_AUTH_USER,
+        user: process.env.SMTP_AUTH_USERNAME,
         pass: process.env.SMTP_AUTH_PASSWORD
     },
     secureConnection: false,
@@ -15,10 +15,14 @@ const options = {
     }
 };
 
+console.log('NodeMailer Configuration:', JSON.stringify(options));
+
 const transporter = nodemailer.createTransport(options);
 
 const compose = (email) => {
     return new Promise((resolve, reject) => {
+        email.from = `BOT <${options.auth.user}>`;
+
         transporter.sendMail(email, (ex, ack) => {
             if (ex) {
                 console.log(`Error sending email: ${ex.message}`);
--- bot/modules/sendmail.spec.js
+++ bot/modules/sendmail.spec.js
@@ -3,12 +3,22 @@
 require('dotenv-extended').load();
 
 const sendmail = require('./sendmail');
+const snail = {
+    name: process.env.SMTP_AUTH_USER,
+    email: process.env.SMTP_AUTH_USERNAME,
+    password: process.env.SMTP_AUTH_PASSWORD
+};
+console.log('Mailer', snail.name, snail.email, snail.password);
 
 // check if the email composer works.
 sendmail.compose({
-    from: 'Bot <email@dress>',
-    to: `KP <email@dress>`,
+    from: `${snail.name} <${snail.email}>`,
+    to: `kosalap@99x.lk`,
     subject: `Your Secret Code: BLAAHBLAAH`,
     text: `You just tryed to sign in with JIRA Journal. Here's your Secret Code: BLAAHBLAAH`,
     html: `You just tryed to sign in with JIRA Journal. Here's your <b>Secret Code: BLAAHBLAAH</b>`
-});
+}).then(() => {
+    console.log(`Dropped an email to ${snail.email}`);
+}).catch(() => {
+    console.log(`Failed sending ${snail.email} with error:`, JSON.stringify(arguments));
+});;
