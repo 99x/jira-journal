cbe173b5ed284f9d80f95771a88486e91801cf64
kosalanuwan <kosala.nuwan@gmail.com>
2017-05-24 23:57:31 +0530

    #6 add help conversation to bot.

--- bot/modules/help.js
+++ bot/modules/help.js
@@ -9,7 +9,7 @@ module.exports = exports = [
             .title('JIRA Journal Bot')
             .text('Your bullet journal - whatever you want to log.')
             .images([
-                builder.CardImage.create(session, 'https://github.com/99xt/jira-journal/blob/master/icon.png')
+                builder.CardImage.create(session, 'https://github.com/99xt/jira-journal/wiki/icon.png')
             ]);
 
         const reply = new builder.Message(session)
@@ -19,6 +19,11 @@ module.exports = exports = [
         session.send(reply);
 
         const { name } = session.message.user;
-        session.endConversation(`Hey ${name}... I'm the JIRA Journal Bot for time reporting. Thanks for adding me. Say 'help' to see what I can do!`);
+
+        session.send(`Hey ${name}... first things first, you need to sign in, in order to do following things`);
+        session.send(`* Type **sign in** so I could log all your time report entries to JIRA.\n* Type **reset** and I will lose all the sweet memories we had together and we can start all over.\n* Type **who am I?** to figure out how much I know about you ;)`);
+        session.send(`Now let's get down to what I can do with your JIRA instances.\n* Type **recent** and I will list all your recent JIRA Tasks.\n* Type **my tasks** and I will list all the JIRA tasks assigned to you.`);
+        session.send(`Now, if you are wondering how to report time, it's like this... You just type what you have been done, and then use the **#tags** to mention the **JIRA Task, Day, and the Time you spent**, then just enter, and I will tell JIRA to note it down :)`);
+        session.endConversation(`Something like this... \n* _I just played with JIRA Journal Bot **#CIN-27 #3/27 #2d3h45m**_ OR \n* _**#CIN-27** Playing around the JIRA Journal Bot source **#yesterday #4.5h**_`);
     }
 ];
