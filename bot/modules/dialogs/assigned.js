175a098d18bf4bf76a72d26e494685103d21f88f
kosalanuwan <kosala.nuwan@gmail.com>
2017-06-29 23:40:06 +0530

    #17 validate current user impersonation.

--- bot/modules/dialogs/assigned.js
+++ bot/modules/dialogs/assigned.js
@@ -3,7 +3,21 @@
 const builder = require('botbuilder');
 const lib = new builder.Library('assigned');
 
+const empty = (arr) => {
+    return !arr || arr.length === 0;
+};
+
 lib.dialog('/', [
+        (session, args, next) => {
+
+            if (!session.userData.profile) {
+                return session.send(`Looks like you haven't signed in.`).replaceDialog('greet:/');
+            } else if (empty.call(this, ...session.userData.jira)) {
+                return session.send(`Looks like you don't have JIRA access.`).replaceDialog('greet:/');
+            }
+
+            next();
+        },
         (session) => {
 
             const {
--- bot/modules/dialogs/recent.js
+++ bot/modules/dialogs/recent.js
@@ -3,7 +3,22 @@
 const builder = require('botbuilder');
 const lib = new builder.Library('recent');
 
+const empty = (arr) => {
+    return !arr || arr.length === 0;
+};
+
 lib.dialog('/', [
+        (session, args, next) => {
+
+            if (!session.userData.profile) {
+                return session.send(`Looks like you haven't signed in.`).replaceDialog('greet:/');
+            } else if (empty.call(this, ...session.userData.jira)) {
+                return session.send(`Looks like you don't have JIRA access.`).replaceDialog('greet:/');
+            }
+
+            next();
+        },
+
         (session) => {
 
             const {
